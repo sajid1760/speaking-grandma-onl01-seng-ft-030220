06c338620195b8b65d49e18bf08c3ef19674150a
Sajid Anjum <sajid1760@gmail.com>
2020-03-03 03:48:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,7 @@ def speak_to_grandma(phrase = "hello")
   puts phrase
  if phrase == phrase.upcase
    puts "NO, NOT SINCE 1938!"
+ end
  # if !(phrase == phrase.upcase) 
   #  puts "HUH?! SPEAK UP, SONNY!"
 # if 3 > 2
