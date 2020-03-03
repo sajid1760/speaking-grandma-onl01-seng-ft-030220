04d84e306fbe957d022727bb22a26606bc6151cf
Sajid Anjum <sajid1760@gmail.com>
2020-03-03 03:33:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-  puts "NO, NOT SINCE 1938!"
+ # if phrase == phrase.upcase
+  # puts "NO, NOT SINCE 1938!"
  # if !(phrase == phrase.upcase) 
   #  puts "HUH?! SPEAK UP, SONNY!"
-#elsif phrase == "I LOVE YOU, GRANDMA!"
- # puts "I LOVE YOU TOO PUMPKIN!"
-#else puts "NO, NOT SINCE 1938!"
+if phrase == "I LOVE YOU, GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+else 
+  puts "NO, NOT SINCE 1938!"
 end
