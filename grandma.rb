9b7c1eddd6f7c445a057a67a259ba1b499dedcdd
Sajid Anjum <sajid1760@gmail.com>
2020-03-03 03:21:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if !(phrase == phrase.upcase)
   puts "HUH?! SPEAK UP, SONNY!"
-else if (phrase == "I LOVE YOU, GRANDMA!")
+else if phrase == "I LOVE YOU, GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 else puts "NO, NOT SINCE 1938!"
 end
